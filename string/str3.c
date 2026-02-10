f7064226bb802321da2af7c702a8977dc1395a84
durgagovindharaj901-glitch <durgagovindharaj901@gmail.com>
2026-02-10 13:27:25 +0530

    Add files via upload

---
+++ string/str3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[20];
+    fgets(str , sizeof(str) , stdin);
+    for(int i = 0; str[i] != '\0';i++){
+        if(str[i] == '@'){
+            break;
+        }
+        printf("%c",str[i]);
+    }
+    return 0;
+}
