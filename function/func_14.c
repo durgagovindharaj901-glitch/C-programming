9b219616c434c8c2816ca97d556d89244d681583
durgagovindharaj901-glitch <durgagovindharaj901@gmail.com>
2026-02-12 00:33:13 +0530

    Add files via upload

---
+++ function/func_14.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void perfect(int n){
+    int sum = 0;
+    for(int i = 1; i <= n/2;i++){
+        if(n%i == 0){
+            sum = sum + i;
+        }
+    }
+    if(sum == n){
+        printf("Perfect number");
+    }else{
+        printf("Not Perfect number");
+    }
+
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    perfect(n);
+    return 0;
+}
---
+++ function/func_15.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int table(int n){
+    for(int i = 1; i <= 10;i++){
+        printf("%d ",i*n); 
+    }
+    return 0;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    table(n);
+    return 0;
+}
